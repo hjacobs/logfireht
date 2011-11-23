6744d55321ab239df9a45e89775dc36832f2f5b3
Henning Jacobs <henning@jacobs1.de>
2011-11-23 22:39:59 +0100

    support network mask for blacklist/whitelist

--- static/js/logfireht.js
+++ static/js/logfireht.js
@@ -28,8 +28,56 @@ function truncate(s, l) {
     return $.esc(s);
 }
 
-function insertFlags() {2
+function insertFlags() {
     $('td.flag').each(function() {
         $(this).html(getFlag($(this).attr('title')));
     });
+}
+
+function isIpInNetwork(ip, net) {
+    var parts = ip.split('.');
+    var iplong = parseInt(parts[0]) << 24 | parseInt(parts[1]) << 16 | parseInt(parts[2]) << 8 | parseInt(parts[3]);
+    var netbits = net.split('/');
+    parts = netbits[0].split('.');
+    var network = parseInt(parts[0]) << 24 | parseInt(parts[1]) << 16 | parseInt(parts[2]) << 8 | parseInt(parts[3]);
+    var mask = 0;
+    for (var i = 32 - parseInt(netbits[1]); i < 32; i++) {
+        mask = mask | (1 << i);
+    }
+
+    return (iplong & mask) == (network & mask);
+}
+
+function prepareBlacklist(list) {
+    var nl = {};
+    $.each(list, function(k, v) {
+        if (k.substring(0, 8) == 'network:') {
+            var parts = k.substring(8).split('.');
+            if (!nl['_net:' + parts[0]]) {
+                nl['_net:' + parts[0]] = [];
+            }
+            nl['_net:' + parts[0]].push([k.substring(8), v]);
+        } else {
+            nl[k] = v;
+        }
+    });
+    return nl;
+}
+
+function getBlacklistEntry(list, ip) {
+    var entry = list['host:' + ip];
+    if (entry) {
+        return entry;
+    }
+    var parts = ip.split('.');
+    var entries = list['_net:' + parts[0]];
+    if (!entries) {
+        return null;
+    }
+    for (var i=0; i<entries.length; i++) {
+        if (isIpInNetwork(ip, entries[i][0])) {
+            return entries[i][1];
+        }
+    }
+    return null;
 }
